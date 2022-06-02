b49f58f4c49bdc71b648d639f1e4731ffb1fed82
Emanuel Gomez <emanueljgomez@gmail.com>
2022-06-02 15:16:20 -0300

    Implements scene flow between main menu and main scene, as well the Exit button functionality to quit the application

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // Required in order to use the SceneManager
+#if UNITY_EDITOR // # is used to indicate instructions for the compiler
+using UnityEditor;
+#endif
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -20,5 +24,24 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+        // INSIDE THE UNITY EDITOR:
+        // 1) File > Build Settings... > (A list of the project scenes will appear, each one associated with a number)
+        // 2) Select the StartButton GameObject and assign this method to it, using the 'On Click()' property under the 'Button' component
+
+    }
+
+    public void Exit()
+    {   
+        // # is used to indicate instructions for the compiler
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
     }
 }
